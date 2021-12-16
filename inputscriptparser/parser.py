35f4fe190edb60342eeb42efec11435ed2cef3f9
takatoh <takatoh.m@gmail.com>
2021-12-16 12:01:38 +0900

    inputscriptparser.py: Remove 'main' function.

--- inputscriptparser/parser.py
+++ inputscriptparser/parser.py
@@ -1,30 +1,4 @@
-from lark import Lark
 from lark.visitors import Interpreter
-from lark.exceptions import UnexpectedInput
-from inputscriptparser.grammer import SCRIPT_GRAMMER
-import sys
-
-
-def main():
-    parser = Lark(SCRIPT_GRAMMER, start="script")
-
-    with open(sys.argv[1], "r") as f:
-        input_data = f.read()
-
-    try:
-        tree = parser.parse(input_data)
-    except UnexpectedInput as e:
-        context = e.get_context(input_data)
-        print(f"Syntax error:  line = {e.line}  column = {e.column}\n")
-        print(context)
-        exit(1)
-
-    #print(tree.pretty())
-    script = ScriptInterpreter().visit(tree)
-    print("SCRIPT")
-    for (cmd, args) in script:
-        print("  COMMAND: " + cmd)
-        print("     ARGS: " + repr(args))
 
 
 class ScriptInterpreter(Interpreter):
@@ -95,7 +69,3 @@ def _flatten(lis):
         else:
             result.append(elem)
     return result
-
-
-
-main()

8df7daae079998cacd027d6dcaeb8f54a1e52aa6
takatoh <takatoh.m@gmail.com>
2021-12-16 12:37:32 +0900

    inputscriptparser/main.py: Use inputscriptparser.Parser.

--- inputscriptparser/main.py
+++ inputscriptparser/main.py
@@ -1,26 +1,15 @@
-from lark import Lark
-from lark.exceptions import UnexpectedInput
-from inputscriptparser.grammer import SCRIPT_GRAMMER
-from inputscriptparser.parser import ScriptInterpreter
+from inputscriptparser.parser import Parser
 import sys
 
 
 def main():
-    parser = Lark(SCRIPT_GRAMMER, start="script")
+    parser = Parser()
 
     with open(sys.argv[1], "r") as f:
         input_data = f.read()
 
-    try:
-        tree = parser.parse(input_data)
-    except UnexpectedInput as e:
-        context = e.get_context(input_data)
-        print(f"Syntax error:  line = {e.line}  column = {e.column}\n")
-        print(context)
-        exit(1)
+    script = parser.parse(input_data)
 
-    #print(tree.pretty())
-    script = ScriptInterpreter().visit(tree)
     print("SCRIPT")
     for (cmd, args) in script:
         print("  COMMAND: " + cmd)

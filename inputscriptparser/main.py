8277131f7f0d9d51faa059727870c91550b847ac
takatoh <takatoh.m@gmail.com>
2022-01-26 08:26:38 +0900

    inputscriptparser/main.py: Use ArgumentParser.

--- inputscriptparser/main.py
+++ inputscriptparser/main.py
@@ -1,19 +1,33 @@
 from inputscriptparser.parsers import Parser
-import sys
+from argparse import ArgumentParser
 
 
 def main():
-    parser = Parser()
+    options = parse_options()
 
-    with open(sys.argv[1], 'r') as f:
+    with open(options.input_file, 'r') as f:
         input_data = f.read()
 
+    parser = Parser()
     script = parser.parse(input_data)
 
     print('SCRIPT')
     for (cmd, args) in script:
         print('  COMMAND: ' + cmd)
         print('     ARGS: ' + repr(args))
+
+
+def parse_options():
+    parser = ArgumentParser(
+        description='Example for InputScriptParser'
+    )
+    parser.add_argument(
+        'input_file',
+        metavar='INPUT_FILE',
+        help='input script file'
+    )
+    args = parser.parse_args()
+    return args
 
 
 

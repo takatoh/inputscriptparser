ae3454cbdf59671b01a08c6f2ca10e88a6b6f067
takatoh <takatoh.m@gmail.com>
2023-05-19 12:35:44 +0900

    inputscriptparser/parsers.py: Remove ScriptInterpreter class.

--- inputscriptparser/parsers.py
+++ inputscriptparser/parsers.py
@@ -1,6 +1,6 @@
 from lark import Lark
 from lark.exceptions import UnexpectedInput
-from lark.visitors import Interpreter, Transformer
+from lark.visitors import Transformer
 from inputscriptparser.common import Keyword, flatten
 from os import path
 
@@ -22,60 +22,9 @@ class Parser():
             print(f'Syntax error:  line = {e.line}  column = {e.column}\n')
             print(context)
             exit(1)
-#        print(tree.pretty())
 
-#        script = ScriptInterpreter().visit(tree)
         script = ScriptTransformer().transform(tree)
         return script
-
-
-class ScriptInterpreter(Interpreter):
-    def script(self, tree):
-        return [self.visit(c) for c  in tree.children]
-
-    def statement(self, tree):
-        (cmd, args1) = self.visit(tree.children[0])
-        args2 = flatten([ self.visit(a) for a in tree.children[1:] ])
-        return (cmd, args1 + args2)
-
-    def line(self, tree):
-        cmd = self.visit(tree.children[0])
-        if len(tree.children) > 1:
-            args = flatten(self.visit(tree.children[1]))
-        else:
-            args = []
-        return (cmd, args)
-
-    def continued(self, tree):
-        return self.visit(tree.children[0])
-
-    def command(self, tree):
-        return tree.children[0]
-
-    def arglist(self, tree):
-        args = [ self.visit(a) for a in tree.children ]
-        return args
-
-    def arg(self, tree):
-        return self.visit(tree.children[0])
-
-    def number(self, tree):
-        return float(tree.children[0])
-
-    def string(self, tree):
-        return tree.children[0].strip('"')
-
-    def keyword(self, tree):
-        return Keyword(str(tree.children[0]))
-
-    def boolean(self, tree):
-        return self.visit(tree.children[0])
-
-    def true(self, tree):
-        return True
-
-    def false(self, tree):
-        return False
 
 
 class ScriptTransformer(Transformer):
